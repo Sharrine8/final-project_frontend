08693c9d44b853d9463cb5c38c3ec1ff7f841529
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2025-05-13 20:37:40 -0500

    fix: drop-down menu and logo color

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -13,6 +13,7 @@ function Navigation({ isLoggedIn, openLoginModal, onLogout }) {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 430);
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
+  const isWhite = isHome || (menuOpen && isMobile);
 
   useEffect(() => {
     const handleResize = () => {
@@ -31,33 +32,58 @@ function Navigation({ isLoggedIn, openLoginModal, onLogout }) {
     >
       <Link
         to="/"
-        className={`nav__logo ${isHome ? "" : "nav__logo_saved-news"}`}
+        className={`nav__logo ${isWhite ? "" : "nav__logo_saved-news"}`}
       >
         NewsExplorer
       </Link>
-      <button
-        onClick={toggleMenu}
-        className={`nav__burger ${menuOpen ? "nav__burger_open" : ""}`}
-      >
-        <span
-          className={`nav__span ${
-            isHome ? "nav__span_type_white" : "nav__span_type_black"
-          }`}
-        />
-        <span
-          className={`nav__span ${
-            isHome ? "nav__span_type_white" : "nav__span_type_black"
-          }`}
-        />
-      </button>
+      {isMobile &&
+        (!menuOpen ? (
+          <button
+            onClick={toggleMenu}
+            className="nav__burger"
+            aria-label="Open menu"
+          >
+            <span
+              className={`nav__span ${
+                isHome ? "nav__span_type_white" : "nav__span_type_black"
+              }`}
+            />
+            <span
+              className={`nav__span ${
+                isHome ? "nav__span_type_white" : "nav__span_type_black"
+              }`}
+            />
+          </button>
+        ) : (
+          <button
+            onClick={toggleMenu}
+            className={`nav__close-btn ${
+              isHome ? "nav__span_type_white" : "nav__span_type_black"
+            }`}
+            aria-label="Close menu"
+          >
+            <span
+              className={`nav__close-icon ${
+                isHome ? "nav__span_type_white" : "nav__span_type_black"
+              }`}
+            ></span>
+            <span
+              className={`nav__close-icon ${
+                isHome ? "nav__span_type_white" : "nav__span_type_black"
+              }`}
+            ></span>
+          </button>
+        ))}
       <ul className={`nav__links ${menuOpen ? "nav__links_open" : ""}`}>
         <li className="nav__li">
           <Link
             to="/"
             className={`nav__link nav__link_type_home ${
               isHome
-                ? "nav__link_white nav__link_white_active"
-                : "nav__link_black"
+                ? `nav__link_type_white ${
+                    isMobile ? "" : "nav__link_type_white_active"
+                  }`
+                : "nav__link_type_black"
             }`}
             onClick={() => setMenuOpen(false)}
           >
@@ -70,8 +96,10 @@ function Navigation({ isLoggedIn, openLoginModal, onLogout }) {
               to="/saved-news"
               className={`nav__link nav__link_saved-news ${
                 isHome
-                  ? "nav__link_white"
-                  : "nav__link_black nav__link_black_active"
+                  ? "nav__link_type_white"
+                  : `nav__link_type_black ${
+                      isMobile ? "" : "nav__link_type_black_active"
+                    }`
               }`}
               onClick={() => setMenuOpen(false)}
             >
@@ -87,7 +115,9 @@ function Navigation({ isLoggedIn, openLoginModal, onLogout }) {
                 onLogout();
               }}
               className={`nav__link nav__btn ${
-                isHome ? "nav__btn_logged-in_white" : "nav__btn_logged-in_black"
+                isWhite
+                  ? "nav__btn_logged-in_white"
+                  : "nav__btn_logged-in_black"
               }`}
             >
               {currentUser.name}
@@ -118,77 +148,3 @@ function Navigation({ isLoggedIn, openLoginModal, onLogout }) {
 }
 
 export default Navigation;
-
-{
-  /* old code */
-}
-//       {isLoggedIn ? (
-//         <ul className="nav__links">
-//           <li className="nav__li">
-//             <Link
-//               to="/"
-//               className={`nav__link nav__link_type_home ${
-//                 isHome
-//                   ? "nav__link_white nav__link_white_active"
-//                   : "nav__link_black"
-//               }`}
-//             >
-//               Home
-//             </Link>
-//           </li>
-//           <li className="nav__li">
-//             <Link
-//               to="/saved-news"
-//               className={`nav__link nav__link_saved-news ${
-//                 isHome
-//                   ? "nav__link_white"
-//                   : "nav__link_black nav__link_black_active"
-//               }`}
-//             >
-//               Saved Articles
-//             </Link>
-//           </li>
-//           <li className="nav__li">
-//             <button
-//               onClick={onLogout}
-//               className={`nav__link nav__btn ${
-//                 isHome ? "nav__btn_logged-in_white" : "nav__btn_logged-in_black"
-//               }`}
-//             >
-//               {currentUser.name}
-//               <img
-//                 alt="logout-icon"
-//                 src={isHome ? logout_white : logout_black}
-//               />
-//             </button>
-//           </li>
-//         </ul>
-//       ) : (
-//         <ul className="nav__links">
-//           <li className="nav__li">
-//             <Link
-//               to="/"
-//               className="nav__link nav__link_type_home nav__link_white_active"
-//             >
-//               Home
-//             </Link>
-//           </li>
-//           <li className="nav__li">
-//             <button
-//               className="nav__link nav__btn"
-//               type="button"
-//               onClick={openLoginModal}
-//             >
-//               Sign in
-//             </button>
-//           </li>
-//         </ul>
-//       )}
-//       {menuOpen && (
-//         <div className="nav__overlay" onClick={() => setMenuOpen(false)}></div>
-//       )}
-//     </nav>
-//   );
-// }
-
-// export default Navigation;
